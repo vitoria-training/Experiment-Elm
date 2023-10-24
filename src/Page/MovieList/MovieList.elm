10c77a275baed55c0337484289369f476f1ced84
KeisukeKatagiri <1997katayangithub@gmail.com>
2023-10-24 16:01:21 +0900

    feat: add Experiment-Elm hide-scrollbar

--- src/Page/MovieList/MovieList.elm
+++ src/Page/MovieList/MovieList.elm
@@ -13,6 +13,7 @@ import Browser.Dom exposing (Viewport)
 import Browser.Events as E
 import Html exposing (..)
 import Html.Attributes as HA exposing (..)
+import Element.Events exposing (onClick)
 
 type Styles
     = None
@@ -22,7 +23,6 @@ type Styles
     | Main
     | Logo
     | Button
-    | TextBox
     | ListTitle
     | MovieRow
     | MovieButton
@@ -46,12 +46,12 @@ stylesheet model =
             , Font.size (model.width / 40)
             ]
         , Style.style Main
-            [ Border.all 1 -- set all border widths to 1 px.
+            [ Border.all 1
             , Color.text Color255.darkCharcoal
             , Color.background Color255.white
             , Color.border Color255.grey
             , Font.size (model.width / 80)
-            , Font.lineHeight 1.3 -- line height, given as a ratio of current font size.
+            , Font.lineHeight 1.3
             ]
         , Style.style Logo
             [ Font.size (model.width / 40)
@@ -65,10 +65,6 @@ stylesheet model =
                 ]
             , Font.size (model.width / 40)
             , Border.all 2
-            ]
-        , Style.style TextBox
-            [ Border.top 3
-            , Font.lineHeight 2
             ]
         , Style.style ListTitle
             [ Font.size (model.width / 50)
@@ -114,106 +110,102 @@ subscriptions _ =
 type alias Movie =
     { movieTitle : String
     , movieUrl : String
-    , movieSortNo : Int-- add id
     }
 type alias Movies =
-    { listSortNo : Int -- add id
+    { listSortNo : Int
     , listTitle : String
+    , startPosition : Int
     , movieData : List Movie
     }
-type alias MovieList =
-    List Movies
 
 type alias Model =
     { width : Float
     , height : Float
-    , movieList : MovieList }
+    , movieList : List Movies }
+
+type StartNo =
+    Int
+
+type alias Cramp =
+    { minPos : Int
+    , maxPos : Int }
 
 -- MovieData
-list1_Movies : { listTitle : String, movieData : List { movieTitle : String, movieUrl : String, movieSortNo : number }, listSortNo : number }
+
+list1_Movies : Movies
 list1_Movies =
     { listSortNo = 0
     , listTitle = "git講習"
+    , startPosition = initStartNo
     , movieData = [
         {movieTitle = "基本コマンド1" 
-            , movieUrl = "Js_8xBDhhwE"
-            , movieSortNo = 0 }
+            , movieUrl = "Js_8xBDhhwE"}
         , {movieTitle = "基本コマンド2"
-            , movieUrl = "eZ9M16REQiQ"
-            , movieSortNo = 1 }
+            , movieUrl = "eZ9M16REQiQ"}
         , {movieTitle = "GitHub_1"
-            , movieUrl = "laz2u--LoTg"
-            , movieSortNo = 2 }
+            , movieUrl = "laz2u--LoTg"}
         , {movieTitle = "GitHub_2"
-            , movieUrl = "tHZ9yR8I81w"
-            , movieSortNo = 3 }
+            , movieUrl = "tHZ9yR8I81w"}
         , {movieTitle = "git内部の仕組み"
-            , movieUrl = "qLyUayBh-T8"
-            , movieSortNo = 4 }
+            , movieUrl = "qLyUayBh-T8"}
         ]
     }
-list2_Movies : { listSortNo : number, listTitle : String, movieData : List { movieTitle : String, movieUrl : String, movieSortNo : number } }
+
+list2_Movies : Movies
 list2_Movies =
     { listSortNo = 1
     , listTitle = "プログラミングパラダイム講習"
+    , startPosition = initStartNo
     , movieData = [
         {movieTitle = "古代ギリシャ-中世編" 
-            , movieUrl = "kvOPZVjBsNA"
-            , movieSortNo = 0 }
+            , movieUrl = "kvOPZVjBsNA"}
         , {movieTitle = "OOP-論理は物質の手足" 
-            , movieUrl = "B3mgmghlEKY"
-            , movieSortNo = 1 }
+            , movieUrl = "B3mgmghlEKY"}
        , {movieTitle = "論理世界への一元化" 
-            , movieUrl = "ClyBlJ8LCQg"
-            , movieSortNo = 2 }
+            , movieUrl = "ClyBlJ8LCQg"}
         , {movieTitle = "論理のみの世界 関数型パラダイム" 
-            , movieUrl = "NGrLa92DHlc"
-            , movieSortNo = 3 }
+            , movieUrl = "NGrLa92DHlc"}
         ]
     }
-list3_Movies : { listSortNo : number, listTitle : String, movieData : List { movieTitle : String, movieUrl : String, movieSortNo : number } }
+
+list3_Movies : Movies
 list3_Movies =
     { listSortNo = 2
     , listTitle = "ゲーム講習"
+    , startPosition = initStartNo
     , movieData = [
         {movieTitle = "【初歩編】第1回" 
-            , movieUrl = "Ht6R3OosXDk"
-            , movieSortNo = 0 }
+            , movieUrl = "Ht6R3OosXDk"}
        , {movieTitle = "【初歩編】第2回" 
-            , movieUrl = "9g-NnkrScng"
-            , movieSortNo = 1 }
+            , movieUrl = "9g-NnkrScng"}
         , {movieTitle = "【初歩編】第3回" 
-            , movieUrl = "f2SZjtkPF2Q"
-            , movieSortNo = 2 }
+            , movieUrl = "f2SZjtkPF2Q"}
         , {movieTitle = "【初歩編】第4回" 
-            , movieUrl = "x4wB8ET-57Y"
-            , movieSortNo = 3 }
+            , movieUrl = "x4wB8ET-57Y"}
         , {movieTitle = "番外編 プランナー編"
-            , movieUrl = "7OvNuawE9ys"
-            , movieSortNo = 4 }
+            , movieUrl = "7OvNuawE9ys"}
         , {movieTitle = "番外編 インストラクター編"
-            , movieUrl = "ZN9ywfx6XS4"
-            , movieSortNo = 5 }
+            , movieUrl = "ZN9ywfx6XS4"}
         ]
     }
-list4_Movies : { listSortNo : number, listTitle : String, movieData : List { movieTitle : String, movieUrl : String, movieSortNo : number } }
+
+list4_Movies : Movies
 list4_Movies =
     { listSortNo = 3
     , listTitle = "System Design講習"
+    , startPosition = initStartNo
     , movieData = [
         {movieTitle = "part1"
-            , movieUrl = "CVHci7zRaw4"
-            , movieSortNo = 0 }
+            , movieUrl = "CVHci7zRaw4"}
         , {movieTitle = "part2"
-            , movieUrl = "62x0qPk7W24"
-            , movieSortNo = 1 }
+            , movieUrl = "62x0qPk7W24"}
         , {movieTitle = "part3"
-            , movieUrl = "Nixcc9fmTqQ"
-            , movieSortNo = 2 }
+            , movieUrl = "Nixcc9fmTqQ"}
         ]
     }
 
 -- Initialize
+initialModel : Model
 initialModel =
     { width = 0
     , height = 0
@@ -224,10 +216,19 @@ initialModel =
         , list4_Movies
         ]
     }
+initStartNo : Int
+initStartNo =
+    0
+
+type Direction
+    = MoveRight
+    | MoveLeft
+
 type Msg
     = NoOp
     | GotInitialViewport Viewport
     | Resize ( Float, Float )
+    | Schroll ( Int, Direction)
 
 -- UPDATE
 setCurrentDimensions : { a | width : b, height : c } -> ( b, c ) -> { a | width : b, height : c }
@@ -246,6 +247,60 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
+        Schroll ( targetListNo, direction ) ->
+            ( setSchroll model ( targetListNo, direction ), Cmd.none )
+
+setSchroll : Model -> ( Int, Direction ) -> Model
+setSchroll model ( targetListNo, direction ) =
+    let
+        setMovieList =
+            List.map ( checkTargetList ( targetListNo, direction ) ) model.movieList
+    in
+        { model | movieList = setMovieList }
+
+checkTargetList : ( Int, Direction ) -> Movies -> Movies
+checkTargetList ( targetListNo, direction ) movie =
+    if movie.listSortNo == targetListNo then
+        setStartPosition ( direction ) movie
+    else
+        movie
+
+setStartPosition : ( Direction ) -> Movies -> Movies
+setStartPosition ( direction ) movie =
+    let
+        nextStartPosition = 
+            selecter movie.startPosition direction
+        
+        maxPos =
+            List.length(movie.movieData)
+    in
+    
+    if checker nextStartPosition maxPos then
+        { movie | startPosition = nextStartPosition } 
+    else
+        movie
+
+selecter : Int -> Direction -> Int
+selecter startPosition direction =
+    case direction of
+        MoveRight ->
+            startPosition + 1
+        MoveLeft ->
+            startPosition - 1
+
+checker : Int -> Int -> Bool
+checker nextStartPosition maxLength =
+    let
+        toDOPos : Int -> Int -> Cramp
+        toDOPos min max =
+            { minPos = min
+            , maxPos = max }
+        cramp = toDOPos 0 maxLength
+    in
+    
+    (cramp.minPos <= nextStartPosition) && ( nextStartPosition < cramp.maxPos )
+
+movieListElement : Model -> Html Msg
 movieListElement model=
     Element.layout (stylesheet model) <|
         column None
@@ -263,6 +318,7 @@ movieListElement model=
             , footerwLayout model
             ]
 
+headerLayout : Model -> Element Styles variation msg
 headerLayout model =
     row Header
         [ spread
@@ -286,14 +342,17 @@ headerLayout model =
                 , Element.button Button [paddingXY 20 0] (Element.text "Contact")
             ] 
         ]
+movieListLayout : Model -> List (Element Styles variation Msg)
 movieListLayout model=
-    [ column None [ yScrollbar][
+    [ column None [][
             column None [][
-                column None []
+                Element.text ("Width : " ++ String.fromFloat(model.width) ++ " Height : " ++ String.fromFloat(model.height))
+                ,column None []
                     (List.map (setMovie model.width model.height) model.movieList)
             ]
         ]
     ]
+setMovie : Float -> Float -> Movies -> Element Styles variation Msg
 setMovie modelWidth modelHeight movies = 
     column None [ paddingBottom 30][
         column ListTitle [ paddingTop 30
@@ -301,16 +360,19 @@ setMovie modelWidth modelHeight movies =
             , paddingRight 0
             , paddingLeft 50][
             Element.text (movies.listTitle)
+            , Element.text ("Sort : " ++ String.fromInt(movies.listSortNo) ++ " start : " ++ String.fromInt(movies.startPosition))
         ]
         , row None [ EA.width fill][
             row None [ paddingXY 15 0][
-                Element.button MovieButton [ paddingTop 0
-                    , paddingBottom 0
-                    , paddingRight 20
-                    , paddingLeft 20
-                    , EA.width <| px ( modelWidth / 40 )
+                Element.button MovieButton [ 
+                    EA.width <| px ( modelWidth / 40 )
                     , EA.height <| px ( modelWidth / 40 ) 
-                    , EA.verticalCenter]
+                    , EA.verticalCenter
+                    , onClick (Schroll (
+                        movies.listSortNo
+                        , MoveLeft)
+                        )
+                    ]
                 (image None [ EA.center
                     , EA.width <| px ( modelWidth / 40 )
                     , EA.height <| px ( modelWidth / 40 ) ]
@@ -319,16 +381,20 @@ setMovie modelWidth modelHeight movies =
                     }
                 )
             ]
-            , row None [ yScrollbar
-                , EA.alignLeft ](List.map (assumption modelWidth modelHeight) movies.movieData)
+            , row None [ EA.alignLeft
+                , clipX](List.map (
+                    assumption modelWidth modelHeight (toFloat movies.startPosition )
+                    ) movies.movieData)
             , row None [ paddingXY 15 0][
-                Element.button MovieButton [ paddingTop 0
-                    , paddingBottom 0
-                    , paddingRight 20
-                    , paddingLeft 20
-                    , EA.width <| px ( modelWidth / 40 )
+                Element.button MovieButton [ 
+                    EA.width <| px ( modelWidth / 40 )
                     , EA.height <| px ( modelWidth / 40 )
-                    , EA.verticalCenter ]
+                    , EA.verticalCenter
+                    , onClick (Schroll (
+                        movies.listSortNo
+                        , MoveRight)
+                        )
+                    ]
                 (image None[ EA.center
                     , EA.width <| px ( modelWidth / 40 )
                     , EA.height <| px ( modelWidth / 40 ) ]
@@ -340,24 +406,29 @@ setMovie modelWidth modelHeight movies =
         ]
     ]
 
-assumption modelWidth modeiHeight movieList=
-    row None [ ][
+setCrampMaxPos : Cramp -> List Movie -> Cramp
+setCrampMaxPos cramp movieDataLength =
+    { cramp | maxPos = List.length movieDataLength }
+
+assumption : Float -> Float -> Float -> Movie -> Element Styles variation msg
+assumption modelWidth modeiHeight startPosition movie =
+    row None [EA.moveLeft (( modelWidth - modelWidth / 10 ) /3 * (startPosition))][
         column None [ spacing 3
-            , paddingXY ( modelWidth / 80 ) ( modeiHeight / 30 )
+            , paddingXY ( modelWidth / 130 ) ( modeiHeight / 30 )
             , EA.center ][
             row MovieRow [ ][
-                Element.text movieList.movieTitle
+                Element.text movie.movieTitle
             ]
             , row MovieRow [][
-                chabgeToElement( videoframe movieList.movieUrl modelWidth modeiHeight )
+                chabgeToElement( videoframe movie.movieUrl modelWidth modeiHeight )
             ]
         ]
     ]
 videoframe : String -> Float -> Float -> Html msg
 videoframe url mainScreenWidth mainScreenheight=
     iframe
-        [ HA.width ( videoWindowWidth mainScreenWidth )--560
-        , HA.height ( videoWindowHeight mainScreenheight )--315
+        [ HA.width ( videoWindowWidth mainScreenWidth )
+        , HA.height ( videoWindowHeight mainScreenheight )
         , HA.src ("https://www.youtube.com/embed/" ++ url)
         , HA.attribute "frameborder" "0"
         , HA.attribute "allowfullscreen" "True"
@@ -375,6 +446,7 @@ chabgeToElement msg =
     -- Change from Html to Element
     html msg
 
+footerwLayout : Model -> Element Styles variation msg
 footerwLayout model =
     row Footer
         [ paddingLeft  (model.width / 50)
